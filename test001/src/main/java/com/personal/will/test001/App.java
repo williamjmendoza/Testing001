20531271356c4fdcfbe50eb8e306c54b3140b3eb
william <william@Inspiron-3543>
2018-12-14 15:30:07 -0300

    Firts commit

---
+++ test001/src/main/java/com/personal/will/test001/App.java
@@ -0,0 +1,13 @@
+package com.personal.will.test001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
